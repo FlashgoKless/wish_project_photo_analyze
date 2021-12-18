90dede3adf279fede07fac02bfb526efaeab4a21
Egor <29282634+FlashgoKless@users.noreply.github.com>
2021-12-19 00:56:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import openpyxl
+from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from os import listdir
+import cmake
+import dlib
+import cv2
+import face_recognition as fr
+import PySimpleGUI as sg
+
+
+layout = [  [sg.Text('Путь до фотографии:')],
+            [sg.Input(), sg.FileBrowse()],
+            [sg.OK(), sg.Cancel()]]
+
+window = sg.Window('Выберите фотографию', layout)
+
+event, jpg_list = window.read()
+value1 = jpg_list[0]
+window.close()
+
+list_name_img = value1
+
+for i in range(len(list_name_img)):
+    for j in range(i+1, len(list_name_img)):
+        if (list_name_img[i][0:-4]) > (list_name_img[j][0:-4]):
+            list_name_img[i],list_name_img[j] = list_name_img[j],list_name_img[i]
+
+
+list_lovations_for_cv = []
+list_encodings = []
+col = 2
+
+xl_file = openpyxl.load_workbook('encodings.xlsx')
+sheet = xl_file.active
+
+
+image_fr = fr.load_image_file(str(list_name_img))
+list_lovations_for_cv.append(fr.face_locations(image_fr))
+list_encodings = fr.face_encodings(image_fr)
+for row in range(128):
+    sheet.cell(column = row+1, row = col).value = list_encodings[0][row]
+col += 1
+
+xl_file.save('encodings.xlsx')
+
+
+df = pd.read_excel('encodings_learn.xlsx')
+
+X = df.drop('target', axis = 1)
+y = df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=0.8,random_state=1)
+
+
+example=pd.read_excel('encodings.xlsx')
+
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+y_pred=model.predict(X_test)
+mae=mean_absolute_error(y_test,y_pred)
+r_sq = model.score(X_train, y_train)
+
+
+
+
+example_df = pd.DataFrame(example)
+
+sg.popup('Ваша оценка', model.predict(example)[0])
+
+'''
+print('---------------------------------------------------------------------------------------|')
+print('mae: ',mae)
+print('coefficient of determination:', r_sq)
+print('---------------------------------------------------------------------------------------|')
+print('Прогноз: ',model.predict(example))
+print('---------------------------------------------------------------------------------------|')
+'''
